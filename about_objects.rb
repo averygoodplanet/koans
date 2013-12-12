9208542652d3d3f2eac089f8f5fe52ecfb16fb02
averygoodplanet <averygoodplanet@gmail.com>
2013-12-12 12:19:37 -0600

    lunch break resume about_objects.rb:29

--- about_objects.rb
+++ about_objects.rb
@@ -2,25 +2,30 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutObjects < Neo::Koan
   def test_everything_is_an_object
-    assert_equal __, 1.is_a?(Object)
-    assert_equal __, 1.5.is_a?(Object)
-    assert_equal __, "string".is_a?(Object)
-    assert_equal __, nil.is_a?(Object)
-    assert_equal __, Object.is_a?(Object)
+    # people say that everything in Ruby is an object
+    assert_equal true, 1.is_a?(Object)
+    assert_equal true, 1.5.is_a?(Object)
+    assert_equal true, "string".is_a?(Object)
+    assert_equal true, nil.is_a?(Object)
+    assert_equal true, Object.is_a?(Object)
   end
 
   def test_objects_can_be_converted_to_strings
-    assert_equal __, 123.to_s
-    assert_equal __, nil.to_s
+    assert_equal "123", 123.to_s
+    assert_equal "", nil.to_s
   end
 
   def test_objects_can_be_inspected
-    assert_equal __, 123.inspect
-    assert_equal __, nil.inspect
+    # inspect method returns a string containing a human-readable representation of obj.
+    assert_equal "123", 123.inspect
+    assert_equal "nil", nil.inspect
   end
 
   def test_every_object_has_an_id
     obj = Object.new
+    # appears that every object has an id, e.g. x.object_id = large integer
+    # and nil.object_id is 8.
+    # x.object_id.class is Fixnum.
     assert_equal __, obj.object_id.class
   end
 
