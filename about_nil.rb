8da8e209f222bd9c8301f1f360f29080248650b1
averygoodplanet <averygoodplanet@gmail.com>
2013-12-12 12:04:21 -0600

    completed about_nil.rb

--- about_nil.rb
+++ about_nil.rb
@@ -2,7 +2,12 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutNil < Neo::Koan
   def test_nil_is_an_object
-    assert_equal __, nil.is_a?(Object), "Unlike NULL in other languages"
+    # eveything in Ruby is an object http://lukaszwrobel.pl/blog/ruby-is-nil
+    # nil is an object of class NilClass
+    # therefore:
+    # nil.is_a?(Object) => true
+    # nil.class => NilClass
+    assert_equal true, nil.is_a?(Object), "Unlike NULL in other languages"
   end
 
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
@@ -12,19 +17,23 @@ class AboutNil < Neo::Koan
     begin
       nil.some_method_nil_doesnt_know_about
     rescue Exception => ex
-      # What exception has been caught?
-      assert_equal __, ex.class
+      # What exception has been caught?  exception of class NoMethodError
+      assert_equal NoMethodError, ex.class
 
       # What message was attached to the exception?
-      # (HINT: replace __ with part of the error message.)
-      assert_match(/__/, ex.message)
+      # some_method_nil_doesnt_know_about
+      # assert_match(pattern, string, message="")
+      assert_match(/some_method_nil_doesnt_know_about/, ex.message)
     end
   end
 
   def test_nil_has_a_few_methods_defined_on_it
-    assert_equal __, nil.nil?
-    assert_equal __, nil.to_s
-    assert_equal __, nil.inspect
+    # Only the object nil responds true to nil?
+    assert_equal true, nil.nil?
+    # .to_s returns a string
+    assert_equal "", nil.to_s
+    # .inspect returns a string containing a human-readable representation of obj.
+    assert_equal "nil", nil.inspect
 
     # THINK ABOUT IT:
     #
@@ -33,6 +42,8 @@ class AboutNil < Neo::Koan
     # or
     #    obj == nil
     # Why?
+    # It appears that x = nil  =.> nil
+    # and x.nil? => true
   end
 
 end
